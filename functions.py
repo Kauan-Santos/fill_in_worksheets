7422af7d5ffe8dce4cc83ccf5a4bf70284512439
Kauan-Santos <kasantos.business@gmail.com>
2022-03-15 08:01:19 -0300

    Versão 1 das funções e exemplos do main

--- functions.py
+++ functions.py
@@ -2,22 +2,20 @@
 
 from openpyxl import load_workbook
 
-wb_example = load_workbook('Example')
-wb_conditions = load_workbook('Conditions')
+wb_example = load_workbook('Example.xlsx')
+wb_conditions = load_workbook('Conditions.xlsx')
 
 ws1_example = wb_example['Plan1']
 ws1_conditions = wb_conditions['Plan1']
 
 
 def value_in_cell(search, status):
-    """Está função tem o objetivo de retornar se o valor é igual ou diferente de 5
-
-        parâmetros = 1 (Númerico do tipo inteiro)
+    """Está função tem o objetivo de 
     """
     for cell in ws1_example['A']:
         if search in str(cell.value) and ws1_example['C' + str(cell.row) == None]:
             ws1_example['C' + str(cell.row)] = status
-        if search in str(cell.value) and ws1_example['C' + str(cell.row) != None]:
+        elif search in str(cell.value) and ws1_example['C' + str(cell.row) != None]:
             print({search} + 'Value status populated with: ' + ws1_example['C' + str(cell.row)])
 
 
@@ -35,6 +33,15 @@ def equal_value_options(value1, value2, status):
             ws1_example['C' + str(cell.row)] = status
 
 
-def value_in_worksheet(status):
-    #define
-    pass
+def value_in_worksheet(status, column):
+    count = 1
+    value_in_column = "A1"
+    while ws1_conditions.max_row >= count:
+        for cell in ws1_example[column]:
+            if str(ws1_conditions[value_in_column].value) == str(cell.value):
+                row = 'C' + str(cell.row)
+                value_in_column = 'A' + str(count)
+                ws1_example[row] = status
+            else:
+                value_in_column = 'A' + str(count)
+        count = count + 1
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from functions import preenchimento
+from functions import *
 
-print(preenchimento(5))
+value_in_cell('2000', 'Old one')
+value_in_cell('2003', 'Old one')
+value_in_cell('2008', 'Old one')
 
+value_is_equal('Hyper-V 2016', 'Hyper-V')
+
+equal_value_options('AIX 7.1.0.0 TL5', 'AIX 7.1.0.0 TL4', 'AIX 4 ou 5')
+
+value_in_worksheet('working days', 'A')
+
+wb_example.save('Example.xlsx')
