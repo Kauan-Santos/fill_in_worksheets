83f8e78759101354b70992b3c80d82e91b8d69c8
Kauan-Santos <kasantos.business@gmail.com>
2022-03-17 21:32:06 -0300

    Definido função main em main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from functions import *
 
-value_in_cell('2000', 'Old one')
-value_in_cell('2003', 'Old one')
-value_in_cell('2008', 'Old one')
 
-value_is_equal('Hyper-V 2016', 'Hyper-V')
+def main(): 
+    value_in_cell('2000', 'Old one')
+    value_in_cell('2003', 'Old one')
+    value_in_cell('2008', 'Old one')
 
-equal_value_options('AIX 7.1.0.0 TL5', 'AIX 7.1.0.0 TL4', 'AIX 4 ou 5')
+    value_is_equal('Hyper-V 2016', 'Hyper-V')
 
-value_in_worksheet('working days', 'A')
+    equal_value_options('AIX 7.1.0.0 TL5', 'AIX 7.1.0.0 TL4', 'AIX 4 ou 5')
 
-wb_example.save('Example.xlsx')
+    value_in_worksheet('working days', 'A')
+
+    wb_example.save('Example.xlsx')
+
+
+if __name__ == '__main__':
+    main()
