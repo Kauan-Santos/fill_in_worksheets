ebd049b44f144810786d4307ad01f9140ae62b13
Kauan-Santos <kasantos.business@gmail.com>
2022-03-21 19:13:26 -0300

    Adicionado documentação do módulo Main

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+"""Esse módulo tem como objetivo buscar e realizar as funções do módulo functions.py."""
+
+
 from functions import *
 
 
 def main(): 
+    """Esta função tem o objetivo enviar os parâmetros para as funções,
+    para preencher a planilha 'Example.xlsx',
+    de acordo com os parâmetros passados para a cada função abaixo.
+
+    Os parâmetros podem substituidos de acordo com sua necessidade e documento a ser preenchido.
+    """
     value_in_cell('2000', 'Old one')
     value_in_cell('2003', 'Old one')
     value_in_cell('2008', 'Old one')
